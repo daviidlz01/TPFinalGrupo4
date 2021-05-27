52c856da07a0cc53f4a2b4643783cd7708ccadc7
daviid <deyvi@DESKTOP-34T29B1>
2021-05-27 17:41:00 -0300

    inicio

---
+++ src/main/java/ar/edu/unju/edm/TpFinalEquipo4Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalEquipo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalEquipo4Application.class, args);
+	}
+
+}
